44b82dc95c69414a94d2e9e2939fdb32ad61ad30
Shadowwolf1728 <fountainjustin2@gmail.com>
2023-06-16 11:16:15 -0400

    main.js modified

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -1,1 +1,10 @@
-const myImage = document.querySelector
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/Size 256x256 Testimage.png") {
+        myImage.setAttribute("src", "images/256x256 image of me.jpg");
+    } else {
+        myImage.setAttribute("src", "images/Size 256x256 Testimage.png")
+    }
+};
