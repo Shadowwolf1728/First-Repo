436bc8e247d50b464c1f5d066563b5a13bd3f9df
Shadowwolf1728 <fountainjustin2@gmail.com>
2023-06-17 09:55:07 -0400

    image finally turned into a 256 by 256

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -3,7 +3,7 @@ const myImage = document.querySelector("img");
 myImage.onclick = () => {
     const mySrc = myImage.getAttribute("src");
     if (mySrc === "images/Size 256x256 Testimage.png") {
-        myImage.setAttribute("src", "images/256x256 image of me.jpg");
+        myImage.setAttribute("src", "images/256x256 image of me.png");
     } else {
         myImage.setAttribute("src", "images/Size 256x256 Testimage.png")
     }
